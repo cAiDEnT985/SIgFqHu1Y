a56d3270ab5cd51390194c50dbcf99c2da0b0131
cAiDEnT985 <hourly_odium.0n@icloud.com>
2025-05-30 21:53:15 +0100

    Add test cases for edge cases in the file

--- 0aYP/yoxHG/Wnm/dVm/university-resources_qC9iLj_1747948331.log.py
+++ 0aYP/yoxHG/Wnm/dVm/university-resources_qC9iLj_1747948331.log.py
@@ -16,7 +16,6 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 # load the dataset (local path)
 url = "data.csv"
-# feature names
 features = ["MDVP:Fo(Hz)","MDVP:Fhi(Hz)","MDVP:Flo(Hz)","MDVP:Jitter(%)","MDVP:Jitter(Abs)","MDVP:RAP","MDVP:PPQ","Jitter:DDP","MDVP:Shimmer","MDVP:Shimmer(dB)","Shimmer:APQ3","Shimmer:APQ5","MDVP:APQ","Shimmer:DDA","NHR","HNR","RPDE","DFA","spread1","spread2","D2","PPE","status"]
 dataset = pandas.read_csv(url, names = features)
 
